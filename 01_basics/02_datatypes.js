cdb22f611880f0289c18ee41c5acd4956bfb255d
Darshit-19 <123617669+Darshit-19@users.noreply.github.com>
2023-10-18 18:04:24 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as new version
+
+// alert(3+3)  //we are using nodejs,not browser
+
+let name = "darshit"
+let age = 20 
+let isLoggedIn = false
+let state = null
+
+//number => 2 to power 53
+// bigint 
+// string => " "
+// boolean => true/false
+//null =>stand alone value
+//undefined => no value assigned
+//symbol => unique
+
+
+//object
+
+console.log(typeof"darshit");
+console.log(typeof age);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
