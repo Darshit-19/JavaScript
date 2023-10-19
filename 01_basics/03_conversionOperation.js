325ac961354e133d2356bf873dd77854b9f1cfa3
Darshit-19 <123617669+Darshit-19@users.noreply.github.com>
2023-10-19 10:20:18 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+//let score = "33"
+//let score = "33abc"
+//let score = null
+//let score = undefined
+let score = "darshit"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33" => 33
+//"33abc" =>NaN
+// true => 1 ,fasle => 0
+
+//let isLoggedIn =1
+//let isLoggedIn =""
+let isLoggedIn ="darshit"
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+//1 => true ,0 =>false
+// "" => false
+// "darshit" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
